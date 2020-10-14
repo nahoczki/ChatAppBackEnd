237496025c185c9d03c8fdc076015e0da66d9e63
m4nz <nnahoczki@gmail.com>
2020-10-13 21:16:18 -0500

    Fixed error handling, returns json object

--- routes/auth.js
+++ routes/auth.js
@@ -39,7 +39,7 @@ router.post('/register', async (req, res) => {
 router.post('/login', async (req, res) => {
     //Validation
     const { error } = loginValidation(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+    if (error) return res.status(400).send(handleErrorMsg(400, error.details[0].message));
 
     //Check if user exists
     const user = await User.findOne({email: req.body.email});
