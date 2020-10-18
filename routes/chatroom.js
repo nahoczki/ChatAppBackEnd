49c2886fd621eadd264ec7ca1d6a75dbc5659811
Zoli Nahoczki <nnahoczki@gmail.com>
2020-10-18 18:39:43 -0500

    Refined ChatRoom endpoints

--- routes/chatroom.js
+++ routes/chatroom.js
@@ -13,9 +13,19 @@ router.get('/', verify, async (req, res) => {
         const requestingUserId = jwt.decode(req.header("auth-token"))._id;
 
         const requestingUser = await User.findOne({ _id: requestingUserId });
+        const chatRooms = requestingUser.chatrooms;
+
+        let chatRoomArr = [];
+
+        for (let chatRoomId of chatRooms) {
+            const chatRoom = await ChatRoom.findOne({_id: chatRoomId});
+
+            chatRoomArr.push(chatRoom);
+        }
+
 
         res.status(200).json({
-            chatrooms: requestingUser.chatrooms
+            chatrooms: chatRoomArr
         });
     } catch (e) {
         res.status(400).send(handleErrorMsg(400, "Internal Error"));
@@ -28,15 +38,16 @@ router.post('/', verify, async (req, res) => {
     const { error } = roomValidation(req.body);
     if (error) return res.status(400).send(handleErrorMsg(400, error.details[0].message));
 
+    const requestingUserId = jwt.decode(req.header("auth-token"))._id;
+    const requestingUser = await User.findOne({ _id: requestingUserId });
+
+    if (requestingUserId === req.body.recipient) return res.status(400).send(handleErrorMsg(400, "Cannot make a chatroom with yourself"))
+
+    // Get and check if recipient exists in db
+    const recipient = await User.findOne({ _id: req.body.recipient });
+    if (!recipient) return res.status(400).send(handleErrorMsg(400, "User does not exist"));
+
     try {
-        const requestingUserId = jwt.decode(req.header("auth-token"))._id;
-        const requestingUser = await User.findOne({ _id: requestingUserId });
-
-        if (requestingUserId === req.body.recipient) return res.status(400).send(handleErrorMsg(400, "Cannot make a chatroom with yourself"))
-
-        // Get and check if recipient exists in db
-        const recipient = await User.findOne({ _id: req.body.recipient });
-        if (!recipient) return res.status(400).send(handleErrorMsg(400, "User does not exist"))
 
         const cleanedRequesting = {
             _id : requestingUser._id,
@@ -56,8 +67,8 @@ router.post('/', verify, async (req, res) => {
 
         //Save
         const savedChatRoom = await newChatRoom.save();
-        await requestingUser.updateOne({ $addToSet: { chatrooms: [savedChatRoom] },});
-        await recipient.updateOne({ $addToSet: { chatrooms: [savedChatRoom] },});
+        await requestingUser.updateOne({ $addToSet: { chatrooms: [savedChatRoom._id] },});
+        await recipient.updateOne({ $addToSet: { chatrooms: [savedChatRoom._id] },});
 
         res.status(200).send(savedChatRoom);
 
